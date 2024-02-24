c1f8060b1047b2a9b21b083b6a4e29e3d06f1751
kevin-beussman <kevin.beussman@gmail.com>
2024-02-24 05:46:33 -0800

    238

---
+++ 238.py
@@ -0,0 +1,57 @@
+"""238. Product of Array Except Self
+Medium
+Topics
+Companies
+
+Given an integer array nums, return an array answer such that answer[i] is equal
+to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit
+integer.
+
+You must write an algorithm that runs in O(n) time and without using the
+division operation.
+ 
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+
+    2 <= nums.length <= 105
+    -30 <= nums[i] <= 30
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output
+array does not count as extra space for space complexity analysis.)
+"""
+
+from typing import List
+#from functools import lru_cache
+#from collections import defaultdict
+#import heapq
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        for i in range(1, len(nums)):
+            prefix[i] = nums[i - 1] * prefix[i - 1]
+            suffix[-(i + 1)] = nums[-i] * suffix[-i]
+
+        answer = [0] * n
+        for i in range(0, len(nums)):
+            answer[i] = prefix[i] * suffix[i]
+        return answer
+
+def main():
+    test = Solution()
+    print(test.productExceptSelf([1,2,3,4]))
+
+if __name__ == "__main__":
+    main()
